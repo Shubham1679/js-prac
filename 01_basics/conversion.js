a25a254677934c1415e7fa90305974bf226a6872
Shubham1679 <52618055+Shubham1679@users.noreply.github.com>
2025-05-10 08:18:05 +0000

    conversion number

---
+++ 01_basics/conversion.js
@@ -0,0 +1,27 @@
+let score = "33a";
+
+//console.log(typeof(score));
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33a"=> NaN
+// true=>1 , false=> 0
+
+let isLoggedIn = "Shubham"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false;
+//"" => false, 
+// "Shubham"=>true;
+
+let someNumber = 33;
+
+
+
+
