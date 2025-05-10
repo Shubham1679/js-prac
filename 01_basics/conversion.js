a6ef2645cd8a5110701b52dd05920c5386bfd4d8
Shubham1679 <52618055+Shubham1679@users.noreply.github.com>
2025-05-10 09:59:33 +0000

    operations

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -23,8 +23,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 39
 
 let str = String(someNumber)
-console.log(str)
-console.log(typeof str)
+//console.log(str)
+//console.log(typeof str)
+
+/********************  operations   ******************** */
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+num1 = num2 = num3 = 2 + 2 ;
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
