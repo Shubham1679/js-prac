4cf37051f8a1d6b356e4079222cb13c1525158ba
Shubham1679 <52618055+Shubham1679@users.noreply.github.com>
2025-05-13 05:48:54 +0000

    array slice/splice

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,4 +1,31 @@
 // Arrays
 
 const myArr = [0,1,2,3,4]
-const myHeros = []
+const myHeros = ["ironman" , "Batman"]
+
+const myArray2 = new Array(1,2,3,4)
+// console.log(myArr[1])
+
+// Array Methods
+// myArr.push(5)
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// // console.log(myArr.indexOf(3));
+
+// const newArray = myArr.join()
+
+// console.log(myArr)
+// console.log(typeof newArray)
+
+//Slice  Splice
+
+console.log("A" , myArr)
+
+const myn1 = myArr.slice(0,3)
+
+console.log(myn1)
