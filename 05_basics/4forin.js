4a1d554754931b12c60cccc31d8bb967f82dd75e
Shubham1679 <52618055+Shubham1679@users.noreply.github.com>
2025-05-16 05:28:04 +0000

    forin loops

---
+++ 05_basics/4forin.js
@@ -0,0 +1,32 @@
+const myObject = {
+    js : 'javascript' ,
+    cpp: 'C++',
+    RB: 'Ruby' ,
+    Swift: 'Swift by Apple'
+}
+
+for (const key in myObject) {
+    
+        // console.log(key)
+        // console.log(myObject[key])
+        // console.log(`${key} shortcut for ${myObject[key]} `)
+    }
+
+    const programming = ["js" , "cpp" , "java" , "py"]
+
+    for (const key in programming) {
+        //   console.log(key)
+        //   console.log(programming[key])
+        }
+    
+const map = new Map()
+
+map.set("IN" , "India")
+map.set("USA" , "United States Of America")
+map.set("UK" , "United Kingdom")
+
+// for (const key in map) {
+//    console.log(key)
+// }
+
+
